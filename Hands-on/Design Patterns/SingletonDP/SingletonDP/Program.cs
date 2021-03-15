de384bc0eba088cc596392a44e420d63194243e9
VRAJ SHAH <vrajshah363@gmail.com>
2021-03-15 19:19:45 +0530

    Abstract Factory and Singleton Design Pattern Handson

---
+++ Hands-on/Design Patterns/SingletonDP/SingletonDP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SingletonDP
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            DBConn instance1 = DBConn.getInstance();
+            DBConn instance2 = DBConn.getInstance();
+            if (instance1 == instance2)
+                Console.WriteLine($"Both the instance are same with \n instance1 hash value {instance1.GetHashCode()} \n instance2 hash value {instance2.GetHashCode()}");
+            Console.ReadLine();
+        }
+    }
+}
